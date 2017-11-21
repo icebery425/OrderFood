208559af7f3b7d72552a889b6b09dafcf75510c8
icebery425 <zhihu_425@163.com>
2017-11-22 00:03:50 +0800

    add reserve page

--- OrderFood/client/wxOrder/pages/reserve/reserve.js
+++ OrderFood/client/wxOrder/pages/reserve/reserve.js
@@ -1,5 +1,17 @@
 Page({
     data: {
+      address:'深圳南山高新园',
+      category:[
+        { "id": 1, "name": "两荤一素"},
+        { "id": 2, "name": "三荤一素"},
+        { "id": 3, "name": "品质套餐" },
+        { "id": 4, "name": "饮料酒水" }
+      ],
+      sku:[
+        { "id": 1, "name": "叉烧饭" },
+        { "id": 2, "name": "烧鸭饭" },
+        { "id": 3, "name": "鸡排饭" },
+      ]
     },
 
    onLoad: function () {
