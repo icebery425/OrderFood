56545cf8c23d52553d8614ba79a00f2546193f75
icebery425 <zhihu_425@163.com>
2017-11-23 20:19:11 +0800

    modify reserve function

--- OrderFood/client/wxOrder/pages/reserve/reserve.js
+++ OrderFood/client/wxOrder/pages/reserve/reserve.js
@@ -1,6 +1,8 @@
 Page({
     data: {
       address:'深圳南山高新园',
+      reduceIcon:"/images/dianc_list_ic_reduce.png",
+      addIcon: "/images/dianc_list_ic_plus.png",
       category:[
         { "id": 1, "name": "两荤一素"},
         { "id": 2, "name": "三荤一素"},
@@ -8,9 +10,9 @@ Page({
         { "id": 4, "name": "饮料酒水" }
       ],
       sku:[
-        { "id": 1, "name": "叉烧饭", "avatar":"/images/img_chas.png", "desc":"叉烧+精致小炒+时蔬+米饭" },
-        { "id": 2, "name": "烧鸭饭", "avatar": "/images/img_jipai.png", "desc": "烧度+精致小炒+时蔬+米饭"},
-        { "id": 3, "name": "鸡排饭", "avatar": "/images/img_shaoya.png", "desc": "叉烧+精致小炒+时蔬+米饭"},
+        { "id": 1, "name": "叉烧饭", "avatar":"/images/img_chas.png", "price":12.00, "desc":"叉烧+精致小炒+时蔬+米饭" },
+        { "id": 2, "name": "烧鸭饭", "avatar": "/images/img_jipai.png", "price": 13.00, "desc": "烧度+精致小炒+时蔬+米饭"},
+        { "id": 3, "name": "鸡排饭", "avatar": "/images/img_shaoya.png", "price": 14.00, "desc": "叉烧+精致小炒+时蔬+米饭"},
       ]
     },
 
