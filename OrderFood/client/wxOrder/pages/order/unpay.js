a94aecafa9651d63bce2ea2bea1b6faa09dd3c2f
icebery425 <zhihu_425@163.com>
2017-11-29 18:23:04 +0800

    modify unpay pages

--- OrderFood/client/wxOrder/pages/order/unpay.js
+++ OrderFood/client/wxOrder/pages/order/unpay.js
@@ -1,5 +1,11 @@
 Page({
   data: {
+    orderItems:[
+      { "id": 1, "name": "叉烧饭", "avatar": "/images/img_chas.png", "price": 12.00, "quantity":10, "desc": "叉烧+精致小炒+时蔬+米饭" },
+      { "id": 2, "name": "烧鸭饭", "avatar": "/images/img_jipai.png", "price": 13.00, "quantity": 10,"desc": "烧度+精致小炒+时蔬+米饭" },
+      { "id": 3, "name": "鸡排饭", "avatar": "/images/img_shaoya.png", "price": 14.00, "quantity": 10,"desc": "叉烧+精致小炒+时蔬+米饭" },
+    ],
+    totalPrice: 20.00,
   },
 
   onLoad: function () {
