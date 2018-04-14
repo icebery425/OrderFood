5468f70be90316a872816c3ebbba11078e50a762
icebery425 <zhihu_425@163.com>
2018-04-14 22:43:04 +0800

    fix WEIXIN pay bug

--- OrderFood/client/wxOrder/pages/order/unpay.js
+++ OrderFood/client/wxOrder/pages/order/unpay.js
@@ -1,4 +1,5 @@
 var network = require('../../common/netWork.js')
+
 Page({
   data: {
     orderItems:[],
@@ -63,7 +64,7 @@ Page({
 
 
     this.setData({
-      supervisorPhone: e.detail.value
+      address: e.detail.value
     })
   }, 
 
@@ -76,7 +77,7 @@ Page({
 
   orderCommit: function () {
 
-    if (this.data.supervisor == null || this.data.supervisor == '' || this.data.supervisor !== undefined) {
+    if (this.data.supervisor == null || this.data.supervisor == '' || this.data.supervisor == undefined) {
       wx.showToast({
         title: '请输入联系人',
         icon: 'none',
@@ -85,7 +86,7 @@ Page({
       return;
     }
 
-    if (this.data.supervisorPhone == null || this.data.supervisorPhone == '' || this.data.supervisorPhone !== undefined) {
+    if (this.data.supervisorPhone == null || this.data.supervisorPhone == '' || this.data.supervisorPhone == undefined) {
       wx.showToast({
         title: '请输入手机号码',
         icon: 'none',
@@ -94,7 +95,7 @@ Page({
       return;
     }
 
-    if (this.data.address == null || this.data.address == '' || this.data.address !== undefined) {
+    if (this.data.address == null || this.data.address == '' || this.data.address == undefined) {
       wx.showToast({
         title: '请输入地址',
         icon: 'none',
@@ -139,6 +140,35 @@ Page({
           console.log('doOrder request result: ', res)
           var list = res.data;
           
+          var payparam = res.data;
+          if (payparam !== null) {    
+            console.log('appid'+res.data.appId + 
+                '\n timeStamp: ' + res.data.timeStamp + 
+                '\n nonceStr: ' + res.data.nonceStr +
+                '\n package:' + res.data.package +
+                '\n signType: ' +res.data.signType +
+              '\n paySign: ' + res.data.paySign);
+
+            wx.requestPayment({
+              'timeStamp': res.data.timeStamp + '',
+              'nonceStr': res.data.nonceStr,
+              'package': res.data.package,
+              'signType': 'MD5',
+              'paySign': res.data.paySign,
+              'success': function (res) {
+                  console.log("###### pay succeed #######");
+                  //searchPayResult(succee, fail);
+                  //dealTimeOut();
+              },
+
+              'fail': function (res) {
+                console.log("###### pay failed #######");
+                console.log(res);
+                //fail(res);
+              }
+            })
+
+          }
         }
       },
       function (res) {
@@ -146,5 +176,73 @@ Page({
         console.log('doOrder request failed: ' + res)
       });
 
+  },
+
+//微信、余额支付结果查询
+searchPayResult:function (succee, fail) {
+
+    gT1 = setInterval(function () {
+      var params = {
+        orderNumber: gOrderId,
+      };
+
+      wx.showLoading({
+        title: '支付状态查询',
+      })
+      netWork.request(0, netWork.apilist.getPayStatus, params,
+        function (res) {
+          console.log('支付结果', res);
+          wx.hideLoading();
+          if (res.status) {
+            console.log("查询支付结果成功");
+            if (res.data.info.return_code == true) {
+
+              succee('支付成功');
+
+            } else if (paystatus == 'REFUND') {
+
+              fail('转入退款');
+
+            } else if (paystatus == 'NOTPAY') {
+
+              fail('未支付');
+
+            } else if (paystatus == 'CLOSED') {
+
+              fail('已关闭');
+
+            } else if (paystatus == 'REVOKED') {
+
+              fail('已撤销');
+
+            } else if (paystatus == 'USERPAYING') {
+
+              console.log('用户支付中...');
+
+            } else if (paystatus == 'PAYERROR') {
+
+              fail('支付失败');
+
+            } else if (paystatus == 'OTHOR') {
+
+              fail('支付异常');
+            }
+            clearTimeOut();
+
+          } else {
+
+            fail('查询支付结果失败');
+            clearTimeOut();
+          }
+        },
+        function () {
+          fail('查询支付结果失败');
+          wx.hideLoading();
+          clearTimeOut();
+        })
+    }, 2000)
   }
+
+
+  
 })
