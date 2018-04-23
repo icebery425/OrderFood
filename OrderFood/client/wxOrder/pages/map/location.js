5c1dae4b6d0a6ee77c2356dec8ec9ecf63d747bf
icebery425 <zhihu_425@163.com>
2018-04-23 23:36:28 +0800

    modify map code

--- OrderFood/client/wxOrder/pages/map/location.js
+++ OrderFood/client/wxOrder/pages/map/location.js
@@ -9,42 +9,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-    Height:500,
     globalData: {
       userInfo: null, 
       locationInfo: null
     },
-    markers: [{
-      iconPath: "/images/songc_nav_ic_add.png",
-      id: 0,
-      latitude: 23.099994,
-      longitude: 113.324520,
-      width: 50,
-      height: 50
-    }],
-    polyline: [{
-      points: [{
-        longitude: 113.3245211,
-        latitude: 23.10229
-      }, {
-        longitude: 113.324520,
-        latitude: 23.21229
-      }],
-      color: "#FF0000DD",
-      width: 2,
-      dottedLine: true
-    }],
+
+    Height: 0,
+    scale: 13,
+    latitude: "",
+    longitude: "",
+    markers: [],
     controls: [{
       id: 1,
-      iconPath: '/images/songc_nav_ic_add.png',
+      iconPath: '/assests/imgs/jian.png',
       position: {
-        left: 0,
-        top: 300 - 50,
-        width: 50,
-        height: 50
+        left: 320,
+        top: 100 - 50,
+        width: 20,
+        height: 20
       },
       clickable: true
-    }]
+    },
+    {
+      id: 2,
+      iconPath: '/assests/imgs/jia.png',
+      position: {
+        left: 340,
+        top: 100 - 50,
+        width: 20,
+        height: 20
+      },
+      clickable: true
+    }
+    ],
+    circles: []
   },
 
   regionchange(e) {
@@ -62,38 +60,49 @@ Page({
    */
   onLoad: function (options) {
     var _this = this;
+
     wx.getSystemInfo({
       success: function (res) {
         //设置map高度，根据当前设备宽高满屏显示
-        var tempheight = res.windowHeight;
         _this.setData({
+          view: {
             Height: res.windowHeight
+          }
+
         })
-      },
+
+
+
+      }
     })
+
+    wx.getLocation({
+      type: 'gcj02', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      success: function (res) {
+
+        _this.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          markers: [{
+            id: "1",
+            latitude: res.latitude,
+            longitude: res.longitude,
+            width: 50,
+            height: 50,
+            iconPath: "/assests/imgs/my.png",
+            title: "哪里"
+
+          }],
+
+
+        })
+      }
+
+    })
+
   },
 
-  getLocationInfo: function (cb) {
-    var that = this;
-    if (this.globalData.locationInfo) {
-      cb(this.globalData.locationInfo)
-    } else {
-      wx.getLocation({
-        type: 'gcj02', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
-        success: function (res) {
-          that.globalData.locationInfo = res;
-          cb(that.globalData.locationInfo)
-        },
-        fail: function () {
-          // fail
-        },
-        complete: function () {
-          // complete
-        }
-      })
-    }
-  },
-
+ 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
